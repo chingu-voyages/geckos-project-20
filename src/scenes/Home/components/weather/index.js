edacd2db8c75b8cc2b2e1fc8d32f291e121a9c78
jennifersmithuk <jennifersmithuk@gmail.com>
2018-12-11 12:55:31 +0000

    feat: Weather app folder structure #2

---
+++ src/scenes/Home/components/weather/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Weather extends Component {
+
+
+
+  render() {
+
+    return (
+      
+      <div className="app-container weather">
+      <p>weather placeholder</p>
+
+      </div>
+      
+
+    );
+  }
+}
+
+export default Weather
--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import Weather from './components/weather';
 
 class Home extends Component {
   render() {
     return (
+
+      <div className="top-bar">
       <h1>Power Panel</h1>
+
+      <Weather />
+      
+      </div>
     );
   }
 }
