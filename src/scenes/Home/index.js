31a196a096466065e49a9118e3821d03041d31e4
jennifersmithuk <jennifersmithuk@gmail.com>
2018-12-18 15:24:13 +0000

    Add Weather to Home

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Quote from './components/Quote';
 import './styles.scss';
-// import Weather from './components/Weather/Weather';
+import Weather from './components/Weather/Weather';
 
 class Home extends Component {
   render() {
     return (
       <div className="app">
         <h1>Power Panel</h1>
+        <Weather />
         <Quote />
       </div>
     );
