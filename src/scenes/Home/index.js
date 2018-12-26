3be2a923dd060db07ee9942fa532ab1c22c0abe4
ir3ne <irene.tomaini@gmail.com>
2018-12-26 10:51:23 +0100

    remove weather component

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react';
 import Quote from './components/Quote';
 import Clock from './components/Clock';
 import './styles.scss';
-import Weather from './components/Weather';
 
 class Home extends Component {
   render() {
     return (
       <div className="app">
-      <div className="top-row">
-        <h1>Power Panel</h1>
-        <div className="top-right">
-          <Weather />
-        </div>
-        </div>
         <Quote />
         <Clock />
       </div>
