23473f65b67c4b225db098d2bab876b6d0424ce7
jennifersmithuk <jennifersmithuk@gmail.com>
2018-12-28 22:30:43 +0000

    Commit changes to Weather to fix develop

--- src/scenes/Home/components/weather/Weather.js
+++ src/scenes/Home/components/weather/Weather.js
@@ -1,82 +1,126 @@
 import React, { Component } from 'react';
 import './styles.scss';
-import { getCurrentWeather } from '../../../../services/api/weather-api';
-
-//import { getAPIURL } from '../../../../services/api/api';
-//import LatLong from './LatLong';
+import { weatherIcon } from './weatherIcon';
+import WeatherExpanded from './WeatherExpanded';
 
 
 class Weather extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            weather: {},
-            isLoading: true,
-            error: '',
-        };
-
+    this.state = {
+      weather: {},
+      isLoading: true,
+      error: '',
+      lat: '',
+      lon: '',
+      timeOfDay: 1,
+      isShowing: false
     };
+  };
+
+  
+  getCurrentWeather () {
+
+    //Example API call: http://api.openweathermap.org/data/2.5/weather?lat=51&lon=-1&units=metric&type=accurate&mode=json&APPID=YOUR_API_KEY
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=metric&type=accurate&mode=json&APPID=${API_KEY}`;
+    let date = new Date();
+    let timeOfDay = date.getHours();
+    console.log(url);
+    console.log(timeOfDay);
+    return fetch(url)
+    .then(response => response.json())
+    .then((data) =>
+      this.setState({ 
+        timeOfDay: timeOfDay,
+        weather: data,
+        weatherID: data.weather[0].id,
+        weatherDescription: data.weather[0].description,
+        isLoading: false  
+      })
+      )
+    .catch(error => 
+      this.setState({ 
+        error, 
+        isLoading: false 
+      })
+      );
+  }
+  
+
+componentDidMount() {
+
+// Get the current position of the user
+  navigator.geolocation.getCurrentPosition(
+  (position) => {
+    this.setState(
+      (prevState) => ({
+        lat: position.coords.latitude, 
+        lon: position.coords.longitude
+        }), () => { this.getCurrentWeather(); } //Passes geolocation to getCurrentWeather function
+    );
+},
+    (error) => this.setState({ error: error }),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+)
+}
+
+/*
+initial weather should display icon, temp and location and be clickable to expand...
+Icon eg. For code 501 - moderate rain icon = "10d"
+URL is http://openweathermap.org/img/w/10d.png */
 
 
-    async componentDidMount() {
+//Function for opening/closing modal for expanded view
+onToggleOpen = (e) => {
+  this.setState((prevState) =>
+  ({
+   isShowing: !prevState.isShowing
+ })
+)
+}
 
-        try {
-            const response = await getCurrentWeather();
-            console.log('weather response : ', response);
-            const data = await response.json();
-            this.setState({
-                weather: data,
-                isLoading: false
-            });
-        } catch (error) {
-            this.setState({ error: error });
-        }
-        console.log("got results");
-        console.log(this.state.weather);
-        console.log(this.state.weather.weather);
+  render() {
+
+    const { weather, error, isLoading, weatherID, weatherDescription, timeOfDay } = this.state;
+
+      if (error) {
+      return <p>{error.message}</p>;
     }
 
-    /*
-    initial weather should display icon, temp and location and be clickable to expand...
-    Icon eg. For code 501 - moderate rain icon = "10d"
-    URL is http://openweathermap.org/img/w/10d.png */
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    } else {
 
+    return (
+   
+      <div className="weather-app-container weather" onClick={this.onToggleOpen}>
 
-    /* TODOs Add City name when location works */
-
-
-    render() {
-
-        const { weather, error, isLoading } = this.state;
-
-        if (error) {
-            return <p>{error.message}</p>;
-        }
-
-        if (isLoading) {
-            return <p>Loading ...</p>;
-        } else {
-
-            console.log("Weather:: ", weather);
-
-            return (
-
-                <div className="weather-results">
-                    <div>
-                        {/* <img src={`https://openweathermap.org/img/w/${weather[0].icon}}.png`} alt='Icon showing weather' className='weather-icon' /> */}
-                        {/* <p>{Math.round(weather.main.temp)}&#8451;</p>
-                        <p>{weather.name}</p> */}
-
-                        No weather
-                    </div>
-                </div>
-
-
-            );
-        }
-    }
+      <div className="weather-wrapper">
+      <div className="weather-stat">
+      <img className="weather-icon" src={weatherIcon(weatherID, timeOfDay)} alt={weatherDescription}/>
+        <p>{Math.round(weather.main.temp)}&deg;</p>
+      </div>
+      </div>
+      <div className="weather-location-label"> 
+        <p>{weather.name}</p>
+        </div> 
+      <div>
+      
+      {this.state.isShowing &&
+              <WeatherExpanded
+              onToggleOpen={this.onToggleOpen}
+          />
+            }
+            
+        </div>
+      </div>
+   
+    );
+  }
+}
 }
 
 export default Weather
