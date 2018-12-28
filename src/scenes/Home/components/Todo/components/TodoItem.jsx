d42b546944827939b7542ea7ce33ea52b46c629c
Mario Krstevski <mariokrstevski@hotmail.com>
2018-12-28 23:41:31 +0100

    Refactored code

--- src/scenes/Home/components/Todo/components/TodoItem.jsx
+++ src/scenes/Home/components/Todo/components/TodoItem.jsx
@@ -10,47 +10,71 @@ class TodoItem extends Component {
         }
     }
 
-    updateTask = (option) => {
-        if (option === 'delete') {
-            this.props.update({
-                id: this.state.id,
-                list: this.state.list,
-                done: this.state.done,
-                task: this.state.task
-            }, true)
-        }
-        else {
-            this.props.update({
-                id: this.state.id,
-                list: this.state.list,
-                done: this.state.done,
-                task: this.state.task
-            })
-        }
-    }
-
-
     changeEditMode = () => {
         this.setState(prevState => ({
             editMode: !prevState.editMode,
         }))
     }
 
-
-
     listOptions = () => {
         return this.props.lists.filter((listOption) => {
             return listOption !== this.state.list
         }).map((listOption, index) => {
-            return <li key={index} onClick={() => this.props.update({
-                id: this.state.id,
-                list: listOption,
-                done: this.state.done,
-                task: this.state.task
-            })}> {listOption} </li>
+            return <li key={index} onClick={() => this.changeList(listOption)}> {listOption} </li>
         })
     }
+    
+    changeList = (newList) => {
+        
+         this.props.update({
+            id: this.state.id,
+            list: newList,
+            done: this.state.done,
+            task: this.state.task
+        })
+    }
+    deleteTask = () => {
+            this.props.update({
+                id: this.state.id,
+                list: this.state.list,
+                done: this.state.done,
+                task: this.state.task
+            }, true)       
+    }
+
+    enterKeyHandler = (event) => {
+        
+        if (event.key === 'Enter') {
+                        
+            if(event.target.value !== this.state.task){
+                this.props.update({
+                    id: this.state.id,
+                    list: this.state.list,
+                    done: this.state.done,
+                    task: event.target.value
+                });
+                this.setState({ task : event.target.value})
+            }
+            this.changeEditMode();
+    }
+}
+
+    outsideClickHandler = (event) => {
+        if(event.target.value !== this.state.task){
+            this.props.update({
+                id: this.state.id,
+                list: this.state.list,
+                done: this.state.done,
+                task: event.target.value
+            });
+
+            this.setState({ task : event.target.value})
+        }
+        this.changeEditMode();
+    }
+
     render() {
+    
 
         const listOptions = this.listOptions();
 
@@ -59,35 +83,7 @@ class TodoItem extends Component {
                 <span>
                     <input type="checkbox" defaultChecked={this.state.done} name={`input_${this.props.id}`} id={`checkbox_${this.props.id}`} />
                 </span>
-                {this.state.editMode ? <input type="text" defaultValue={this.state.task} onKeyUp={(event)=>{
-                    if (event.key === 'Enter') {
-                        
-                        if(event.target.value !== this.state.task){
-                            this.props.update({
-                                id: this.state.id,
-                                list: this.state.list,
-                                done: this.state.done,
-                                task: event.target.value
-                            });
-                            this.setState({ task : event.target.value})
-                        }
-                        
-                        this.changeEditMode();
-                    }
-                }} onBlur={(event) => {
-                    if(event.target.value !== this.state.task){
-                        this.props.update({
-                            id: this.state.id,
-                            list: this.state.list,
-                            done: this.state.done,
-                            task: event.target.value
-                        });
-
-                        this.setState({ task : event.target.value})
-                    }
-                    this.changeEditMode();
-                }} /> : <span> {this.state.task} </span>}
-
+                {this.state.editMode ? <input type="text" defaultValue={this.state.task} onKeyUp={this.enterKeyHandler} onBlur={this.outsideClickHandler} /> : <span> {this.state.task} </span>}
 
                 <span >
                     <span onClick={this.changeEditMode}> Edit </span>
@@ -95,18 +91,11 @@ class TodoItem extends Component {
                     <span>
                         {listOptions}
                     </span>
-                    <span onClick={() => {
-                        this.props.update({
-                            id: this.state.id,
-                            list: this.state.list,
-                            done: this.state.done,
-                            task: this.state.task
-                        }, true)
-                    }}> Delete </span>
+                    <span onClick={this.deleteTask}> Delete </span>
                 </span>
             </div>
         );
     }
-}
+    }
 
 export default TodoItem;
