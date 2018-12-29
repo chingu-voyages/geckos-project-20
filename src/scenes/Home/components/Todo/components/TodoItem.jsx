0b04f4a03f90fd7c943c0ec2fc43dc2127955a10
Mario Krstevski <mariokrstevski@hotmail.com>
2018-12-29 13:20:30 +0100

    simplified the inputHandle functions

--- src/scenes/Home/components/Todo/components/TodoItem.jsx
+++ src/scenes/Home/components/Todo/components/TodoItem.jsx
@@ -45,24 +45,9 @@ class TodoItem extends Component {
             }, true)       
     }
 
-    enterKeyHandler = (event) => {
-        
-        if (event.key === 'Enter') {
-                        
-            if(event.target.value !== this.state.task && event.target.value !== ''){
-                this.props.update({
-                    id: this.state.id,
-                    list: this.state.list,
-                    done: this.state.done,
-                    task: event.target.value
-                });
-                this.setState({ task : event.target.value})
-            }
-            this.changeEditMode();
-    }
-}
 
-    outsideClickHandler = (event) => {
+
+    inputHandler = (event) => {
         if(event.target.value !== this.state.task && event.target.value !== ''){
             this.props.update({
                 id: this.state.id,
@@ -100,7 +85,13 @@ class TodoItem extends Component {
                 </span>
                 <span className={ this.state.done ? 'done' : ''}>
 
-                {this.state.editMode ? <input type="text" defaultValue={this.state.task} onKeyUp={this.enterKeyHandler} onBlur={this.outsideClickHandler} /> : <span> {this.state.task} </span>}
+                {this.state.editMode ? <input type="text" defaultValue={this.state.task} 
+                onKeyUp={(event) => {
+                    if (event.key === 'Enter') {
+                        this.inputHandler(event);
+                    }
+                }} 
+                onBlur={this.inputHandler} /> : <span> {this.state.task} </span>}
                 </span>
 
                 <span >
