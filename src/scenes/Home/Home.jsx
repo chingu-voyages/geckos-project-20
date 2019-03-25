4bbdb7a464a808ca04b1c9436a6b0c11b84d7d53
Mario Krstevski <Mario.Krstevski@netcetera.com>
2019-03-25 08:52:56 +0100

    Converted state component to pure component

--- src/scenes/Home/Home.jsx
+++ src/scenes/Home/Home.jsx
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './styles.scss';
 
 import { Todo, Background, Weather, Quote, Search, Focus, Clock, Toggle } from './components';
 import { Activator, Content } from './components/home.scmp';
 
-class Home extends Component {
-    render() {
-        return (
-            <div className="main-view">
-                <Background />
-                <div className="app">
-                    <div className="top-row">
-                        <div className="top-left">
-                            <Search />
-                        </div>
-                        <div className="top-right">
-                            <Weather />
-                        </div>
+function Home() {
+    return (
+        <div className="main-view">
+            <Background />
+            <div className="app">
+                <div className="top-row">
+                    <div className="top-left">
+                        <Search />
                     </div>
-                    <div className="center">
-                        <Clock />
-                    </div>
-                    <div className="center-below">
-                        <Focus />
-                    </div>
-                    <div className="bottom">
-                        <Quote />
-                    </div>
-
-                    <div className="bottom-right">
-                        <Toggle>
-                            <Activator>
-                                <div id="todo">Todo</div>
-                            </Activator>
-                            <Content side="above">
-                                <Todo />
-                            </Content>
-                        </Toggle>
+                    <div className="top-right">
+                        <Weather />
                     </div>
                 </div>
+                <div className="center">
+                    <Clock />
+                </div>
+                <div className="center-below">
+                    <Focus />
+                </div>
+                <div className="bottom">
+                    <Quote />
+                </div>
+
+                <div className="bottom-right">
+                    <Toggle>
+                        <Activator>
+                            <div id="todo">Todo</div>
+                        </Activator>
+                        <Content side="above">
+                            <Todo />
+                        </Content>
+                    </Toggle>
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Home;
