f9d9b08b1c6c2e856ce8cb2293bfe2497ef75bae
Mario Krstevski <Mario.Krstevski@netcetera.com>
2019-03-25 09:30:41 +0100

    Refactored Todo with hooks, has minor error

--- src/scenes/Home/components/Todo/Todo.jsx
+++ src/scenes/Home/components/Todo/Todo.jsx
@@ -1,129 +1,116 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './todo.styles.scss';
 import { ListOptions, TodoList } from './components';
 import { initializeTodos } from './repository/todos';
 
-class Todo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            lists: ['main', 'today', 'done'],
-            activeList: 'today',
-            lastId: 2,
-            allTodos: [],
-            filteredTodos: [],
-            todoChanged: true,
-        };
-        this.newTodoInput = null;
-    }
-    render() {
-        return (
-            <div className="todoFeature">
-                <ListOptions
-                    changeList={this.changeActiveList}
-                    lists={this.state.lists}
-                    activeList={this.state.activeList}
-                    allTodos={this.state.allTodos}
-                    needsToUpdate={this.state.todoChanged}
-                />
-                <TodoList filteredTodos={this.state.filteredTodos} update={this.updateTask} lists={this.state.lists} />
+function Todo() {
+    const [activeList, setActiveList] = useState('today');
+    let [lastId, setLastId] = useState(2);
+    const [allTodos, setAllTodos] = useState([]);
+    const [filteredTodos, setFilteredTodos] = useState([]);
+    const [lists, setLists] = useState(['main', 'today', 'done']);
 
-                <form autoComplete="off" onSubmit={this.newTodoHandler}>
-                    <input
-                        className="todoFeature__input"
-                        placeholder="New Todo"
-                        type="text"
-                        name="newTodo"
-                        id="newTodo"
-                        ref={node => (this.newTodoInput = node)}
-                    />
-                </form>
-            </div>
-        );
-    }
-    componentDidMount() {
-        this.setState({ allTodos: initializeTodos() });
-        this.filterTodos();
-    }
+    //are these 2 the same?
+    const [todoChanged, setTodoChanged] = useState(true);
+    const [needsToUpdate, setNeedsToUpdate] = useState(false);
 
-    filterTodos = option => {
+    let newTodoInput = null;
+
+    // Initialized Todo's
+    useEffect(() => {
+        setAllTodos(initializeTodos());
+        filterTodos();
+    }, []);
+
+    const filterTodos = option => {
         if (option === 'done') {
-            this.setState(prevState => ({
-                filteredTodos: [...prevState.allTodos].filter(todo => todo.done),
-            }));
+            setFilteredTodos([...allTodos].filter(todo => todo.done));
         } else {
-            this.setState(prevState => ({
-                filteredTodos: [...prevState.allTodos].filter(todo => todo.list === prevState.activeList),
-            }));
+            setFilteredTodos([...allTodos].filter(todo => todo.list === activeList));
         }
     };
 
-    newTodoHandler = event => {
+    const newTodoHandler = event => {
         event.preventDefault(); // Let's stop this event.
 
-        let newTodo = this.newTodoInput.value;
+        let newTodo = newTodoInput.value;
         if (newTodo !== '') {
-            this.setState(prevState => ({
-                allTodos: [
-                    ...prevState.allTodos,
-                    {
-                        id: prevState.lastId + 1,
-                        list: prevState.activeList,
-                        done: false,
-                        task: newTodo,
-                    },
-                ],
-                lastId: ++prevState.lastId,
-            }));
-
-            this.filterTodos();
-            this.newTodoInput.value = '';
+            setAllTodos([
+                ...allTodos,
+                {
+                    id: lastId + 1,
+                    list: activeList,
+                    done: false,
+                    task: newTodo,
+                },
+            ]);
+            setLastId(++lastId);
+            filterTodos();
+            newTodoInput.value = '';
         }
     };
 
-    changeActiveList = newList => {
+    const changeActiveList = newList => {
         if (newList === 'done') {
-            this.filterTodos('done');
+            filterTodos('done');
         } else {
             // If nothing is changed don't change state / don't rerender
-            if (this.state.activeList !== newList) {
-                let listExists = [...this.state.lists].includes(newList);
+            if (activeList !== newList) {
+                let listExists = [...lists].includes(newList);
 
                 if (!listExists) {
-                    this.setState(prevState => ({
-                        lists: [...prevState.lists, newList],
-                    }));
+                    setLists([...lists, newList]);
                 }
 
-                this.setState({
-                    activeList: newList,
-                });
-                this.filterTodos();
+                setActiveList(newList);
+                filterTodos();
             }
         }
     };
 
-    updateTask = (object, optional) => {
+    const updateTask = (object, optional) => {
         if (optional) {
-            this.setState(prevState => ({
-                allTodos: [...prevState.allTodos].filter(todo => {
+            setAllTodos(
+                [...allTodos].filter(todo => {
                     return todo.id !== object.id;
                 }),
-            }));
+            );
         } else {
-            let prevStateAllTodos = [...this.state.allTodos];
-            let index = this.state.allTodos.findIndex(element => {
+            let prevStateAllTodos = [...allTodos];
+            let index = allTodos.findIndex(element => {
                 return element.id === object.id;
             });
             prevStateAllTodos[index] = object;
-            this.setState({
-                allTodos: prevStateAllTodos,
-                needsToUpdate: !this.state.needsToUpdate,
-            });
+            setAllTodos(prevStateAllTodos);
+            setNeedsToUpdate(!needsToUpdate);
         }
 
-        this.filterTodos();
+        filterTodos();
     };
+
+    return (
+        <div className="todoFeature">
+            <ListOptions
+                changeList={changeActiveList}
+                lists={lists}
+                activeList={activeList}
+                allTodos={allTodos}
+                needsToUpdate={todoChanged}
+            />
+            <TodoList filteredTodos={filteredTodos} update={updateTask} lists={lists} />
+
+            <form autoComplete="off" onSubmit={newTodoHandler}>
+                <input
+                    className="todoFeature__input"
+                    placeholder="New Todo"
+                    type="text"
+                    name="newTodo"
+                    id="newTodo"
+                    ref={node => (newTodoInput = node)}
+                />
+            </form>
+        </div>
+    );
 }
 
 export default Todo;
